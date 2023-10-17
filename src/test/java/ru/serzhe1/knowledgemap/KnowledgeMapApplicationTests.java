a3fb973e8e1069408181e582d2a48900a1878940
s.ladygin <s.ladygin@click-group.ru>
2023-10-17 20:09:21 +0300

    init commit

---
+++ src/test/java/ru/serzhe1/knowledgemap/KnowledgeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serzhe1.knowledgemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
